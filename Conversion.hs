21f03edba3c02799049fe9f13647a0ee864d3013
Brendan Good <brendan@debian.Brendan>
2015-11-06 10:30:05 -0500

    Everything has been defined and doesn't crash when invoked.

--- Conversion.hs
+++ Conversion.hs
@@ -2,38 +2,80 @@ module Conversion where
 import Parse
 import qualified Data.Map.Strict as Map
 
-data NFA = Map (Char) ([NFA]) String  Bool | Nil
+--The empty string is not a valid character hence the necessity of the epsilon
+data Transition = Transition Char | Epsilon | Escaped Char deriving (Ord, Eq)
+
+{-
+An NFA is a map from characters to a list of NFA's and a boolean which
+states whether the node is an ending state
+-}
+
+data NFA = Node {
+    transitions :: (Map.Map (Transition) ([NFA]))
+  , final_state :: Bool
+} | Nil deriving (Ord, Eq)
 
 convert :: Regex -> NFA
 convert regex = 
     case regex of
-        Or (term) (regex) -> convert_or Nil term regex
-        Regex term -> convert_term Nil term
+        Or (term) (regex) -> convert_or term regex Nil
+        Regex term -> convert_term term Nil
 
-convert_or :: NFA -> Term -> Regex -> NFA
-convert_or starting_node term regex =
-    undefined
+convert_or :: Term -> Regex -> NFA -> NFA
+convert_or term regex ending_node=
+    let term_ending_node = Node (Map.singleton Epsilon [ending_node]) False in
+        let term_initial_node = convert_term term term_ending_node in
+            let regex_ending_node = Node (Map.singleton Epsilon [ending_node]) False in
+                let regex_initial_node = convert_regex regex regex_ending_node in
+                    Node (Map.fromList [(Epsilon, [term_initial_node, regex_initial_node])]) False
+                
 
-convert_term :: NFA -> Term -> NFA
-convert_term starting_node (factor:factors) =
-    undefined
+convert_term :: Term -> NFA -> NFA
+convert_term term ending_node =
+    case term of
+        (x:xs) -> let factor_NFA = convert_factor (last term) ending_node in
+                                convert_term (init term) factor_NFA
+        [] -> Nil
 
-kleene_star :: Factor -> NFA
-kleene_star (Starred base) =
-    undefined
+convert_factor :: Factor -> NFA -> NFA
+convert_factor factor ending_node =
+    case factor of
+        (Unstarred base) -> convert_base base ending_node
+        (Starred base) -> kleene_star base ending_node
 
-convert_regex :: NFA -> Regex -> NFA
-convert_regex starting_node regex =
+kleene_star :: Base -> NFA -> NFA
+kleene_star base ending_node =
+    let penultimate_node = Node (Map.singleton Epsilon [ending_node]) False in
+        let base_node = convert_base base penultimate_node in
+            Node (Map.singleton Epsilon [base_node]) False
+        
+
+convert_regex :: Regex -> NFA -> NFA
+convert_regex regex ending_node =
     case regex of
-        Or (regex) (term) -> convert_or starting_node regex term
-        Regex term -> convert_term starting_node term
+        Or (regex) (term) -> convert_or regex term ending_node
+        Regex term -> convert_term term ending_node
 
-convert_base :: NFA -> Base -> NFA
-convert_base starting_node base =
+convert_base :: Base -> NFA -> NFA
+convert_base base ending_node =
     case base of
-        Backslash char -> convert_literal starting_node ("\\"++char:"")
-        Base char -> convert_literal starting_node (char:"")
-        Parenthesized regex -> convert_regex starting_node regex
+        Backslash char -> convert_literal (Escaped char) ending_node
+        Base char -> convert_literal (Transition char) ending_node
+        Parenthesized regex -> convert_regex regex ending_node
 
-convert_literal :: NFA -> String -> NFA
-convert_literal starting_node literal = undefined
+convert_literal :: Transition -> NFA -> NFA
+convert_literal literal ending_node = 
+    case literal of
+        Escaped char -> convert_escaped_character char
+        Transition char -> convert_character char
+
+convert_character :: Char -> NFA
+convert_character char =
+    let ending_node = Node (Map.empty) False in
+     Node (Map.singleton (Transition char) [ending_node]) True
+
+convert_escaped_character :: Char -> NFA
+convert_escaped_character char = 
+    let ending_node = Node (Map.empty) False in
+        let penultimate_node = Node (Map.singleton (Transition char) [ending_node]) True in
+            Node (Map.singleton (Transition '\\') [penultimate_node]) True 
